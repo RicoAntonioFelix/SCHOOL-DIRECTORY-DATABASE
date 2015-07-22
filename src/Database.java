3990a40a4e9535bb125739d72f96396b2d0628a2
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-07-22 00:46:22 -0400

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Rico Antonio Felix
+ * Copyright (C) 2014-2015 Rico Antonio Felix
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
+
+/**
+ * @author Rico Antonio Felix <ricoantoniofelix@yahoo.com>
  */
 
 package com.rico.felix.data;
@@ -27,6 +31,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+
+/*
+ * Platform Dependency
+ */
 import java.util.TreeMap;
 
 /*
@@ -39,69 +47,72 @@ import com.rico.felix.models.Person;
  */
 public class Database
 {
-	/*
-	 * Prevent instantiation
-	 */
-	private Database()
-	{}
+    /*
+     * Prevent instantiation
+     */
+    private Database()
+    {}
 
-	/**
-	 * This method is used to persist runtime data in a file on disk
-	 */
-	public static void save()
-	{
-		try (BufferedOutputStream bos = new BufferedOutputStream(
-				new FileOutputStream("directory.dat"));
-			 ObjectOutputStream oos = new ObjectOutputStream(bos);
-			)
-		{
-			oos.writeObject(Directory.getFilingSystem());
-		}
-		catch (IOException ioe)
-		{
-			ioErrorMessage();
-		}
-	}
+    /**
+     * Persist runtime data in a file on disk
+     *
+     * @return void
+     */
+    public static void save()
+    {
+        try (BufferedOutputStream bos = new BufferedOutputStream(
+                                            new FileOutputStream("directory.dat"));
+             ObjectOutputStream oos = new ObjectOutputStream(bos);
+            )
+        {
+            oos.writeObject(Directory.getFilingSystem());
+        }
+        catch (IOException ioe)
+        {
+            ioErrorMessage();
+        }
+    }
 
-	/**
-	 * This method is used to restore persisted data if any
-	 *
-	 * @return Whether restoration succeeded or not
-	 */
-	@SuppressWarnings("unchecked")
-	public static boolean restore()
-	{
-		try (BufferedInputStream bis = new BufferedInputStream(
-				new FileInputStream("directory.dat"));
-			 ObjectInputStream ois = new ObjectInputStream(bis);
-			)
-		{
-			return Directory.setFilingSystem((TreeMap<String, Person>)ois.readObject());
-		}
-		catch (IOException ioe)
-		{
-			if (Directory.getFilingSystem().isEmpty())
-				; // Application has no records in the database which is okay
-			else
-				ioErrorMessage();
-		}
-		catch (ClassNotFoundException cnfe)
-		{
-			System.err.println("Internal Error: ");
-			cnfe.printStackTrace();
-		}
-		return false;
-	}
+    /**
+     * Restore persisted data if any
+     *
+     * @return boolean - true if restoration succeeded, false otherwise
+     */
+    @SuppressWarnings("unchecked")
+    public static boolean restore()
+    {
+        try (BufferedInputStream bis = new BufferedInputStream(
+                                            new FileInputStream("directory.dat"));
+             ObjectInputStream ois = new ObjectInputStream(bis);
+            )
+        {
+            return Directory.setFilingSystem((TreeMap<String, Person>)ois.readObject());
+        }
+        catch (IOException ioe)
+        {
+            if (Directory.getFilingSystem().isEmpty())
+                ; // Application has no records in the database which is okay
+            else
+                ioErrorMessage();
+        }
+        catch (ClassNotFoundException cnfe)
+        {
+            System.err.println("Internal Error: ");
+            cnfe.printStackTrace();
+        }
+        return false;
+    }
 
-	/**
-	 * This method is used to stream an error message upon i/o error
-	 * and terminate the application
-	 */
-	private static void ioErrorMessage()
-	{
-		System.err.println("I/O Error Occurred Unexpectedly");
-		System.err.println("Application will now terminate");
-		System.exit(-0x1);
-	}
+    /**
+     * Stream an error message upon i/o error and terminate the application
+     *
+     * @return void
+     */
+    private static void ioErrorMessage()
+    {
+        System.err.println("I/O Error Occurred Unexpectedly");
+        System.err.println("Application will now terminate");
+        System.exit(-0x1);
+    }
 
 }
