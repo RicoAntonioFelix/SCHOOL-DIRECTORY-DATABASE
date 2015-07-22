b6539f4c1bc245255e0c0919034e144a0ed68ed4
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-07-22 00:54:30 -0400

    Update Staff.java

--- src/Staff.java
+++ src/Staff.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Rico Antonio Felix
+ * Copyright (C) 2014-2015 Rico Antonio Felix
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,10 @@
  * limitations under the License.
  */
 
+/**
+ * @author Rico Antonio Felix <ricoantoniofelix@yahoo.com>
+ */
+
 package com.rico.felix.models;
 
 /**
@@ -25,63 +29,66 @@ package com.rico.felix.models;
  */
 public final class Staff extends Person
 {
-	/**
-	 * Object used for the serialization mechanism
-	 */
-	private static final long serialVersionUID = -3968916956475875017L;
+    // Field used for the serialization mechanism
+    private static final long serialVersionUID = -3968916956475875017L;
 
-	// Fields for storing fundamental information for an object of this type
-	private String title;
-	private String office;
+    // Fields for storing fundamental information for an object of this type
+    private String title;
+    private String office;
 
-	/**
-	 * Parameterized constructor for an object of this type
-	 *
-	 * @param title     Staff member's title
-	 * @param firstName Staff member's first name
-	 * @param lastName  Staff member's last name
-	 * @param email     Staff member's email address
-	 * @param office    Name of staff member's office
-	 */
-	public Staff(final String title, final String firstName, final String lastName,
-		  final String email, final String office)
-	{
-		super(firstName, lastName, email);
-		this.title = title;
-		this.office = office;
-	}
+    /**
+     * Parameterized constructor for an object of this type
+     *
+     * @param title     - Staff member's title
+     * @param firstName - Staff member's first name
+     * @param lastName  - Staff member's last name
+     * @param email     - Staff member's email address
+     * @param office    - Name of staff member's office
+     */
+    public Staff(final String title, final String firstName, final String lastName,
+          final String email, final String office)
+    {
+        super(firstName, lastName, email);
+        this.title  = title;
+        this.office = office;
+    }
 
-	/**
-	 * Retrieve name of office associated with an object of this type
-	 *
-	 * @return Name of office associated with the object
-	 */
-	public String getOffice()
-	{
-		return office;
-	}
+    /**
+     * Retrieve name of office associated with an object of this type
+     *
+     * @return String - Name of office associated with the object
+     */
+    public String getOffice()
+    {
+        return office;
+    }
 
-	/**
-	 * Change the name of office associated with an object of this type
-	 *
-	 * @param newOffice New name of office to associate with the object
-	 */
-	public void setOffice(final String newOffice)
-	{
-		office = newOffice;
-	}
+    /**
+     * Change the name of office associated with an object of this type
+     *
+     * @param newOffice - New name of office to associate with the object
+     *
+     * @return void
+     */
+    public void setOffice(final String newOffice)
+    {
+        office = newOffice;
+    }
 
-	/**
-	 * Retrieve the string representation of an object of this type
-	 *
-	 * @return String representation for this object
-	 */
-	@Override
-	public String toString()
-	{
-		return String.format("Staff Member Name: %s%nE-mail Address: %s%n"
-				+ "Office: %s%n%n", title + " " + getFirstName() + " "
-				+ getLastName(), getEmail(), office);
-	}
+    /**
+     * Retrieve the string representation of an object of this type
+     *
+     * @return String - String representation for this object
+     */
+    @Override
+    public String toString()
+    {
+        return String.format(
+            "Staff Member Name: %s%nE-mail Address: %s%nOffice: %s%n%n",
+            (title + " " + getFirstName() + " " + getLastName()),
+            getEmail(),
+            office
+        );
+    }
 
 }
