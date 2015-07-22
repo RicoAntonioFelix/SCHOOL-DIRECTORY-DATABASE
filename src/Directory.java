ed02e38992f64cc593cb4535158147816ff49beb
Rico Antonio Felix <ricoantoniofelix@yahoo.com>
2015-07-22 00:47:40 -0400

    Update Directory.java

--- src/Directory.java
+++ src/Directory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Rico Antonio Felix
+ * Copyright 2014-2015 Rico Antonio Felix
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,16 +14,29 @@
  * limitations under the License.
  */
 
+/**
+ * @author Rico Antonio Felix <ricoantoniofelix@yahoo.com>
+ */
+
 package com.rico.felix.data;
 
 /*
  * Platform Dependencies
  */
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+
+/*
+ * Platform Dependency
+ */
+import java.util.function.Predicate;
+
+/*
+ * Platform Dependency
+ */
+import java.util.stream.Collectors;
 
 /*
  * Local Dependencies
@@ -31,122 +44,113 @@ import java.util.TreeMap;
 import com.rico.felix.models.Person;
 import com.rico.felix.models.Student;
 
-/*
- * Local Dependency
- */
-import com.rico.felix.auxiliary.Predicate;
-
 /**
  * This class is used as an in-memory database for managing the application's
  * data.
  */
 public class Directory
 {
-	/**
-	 * Object used for managing the application's data
-	 */
-	private static Map<String, Person> filingSystem = new TreeMap<>();
+    /**
+     * Object used for managing the application's data
+     */
+    private static Map<String, Person> filingSystem = new TreeMap<>();
 
-	/*
-	 * Prevent instantiation
-	 */
-	private Directory()
-	{}
+    /*
+     * Prevent object instantiation
+     */
+    private Directory()
+    {}
 
-	/**
-	 * Method used to add a record to the database
-	 *
-	 *@param person Record consisting of information pertaining to a person object
-	 *              to add to the database
-	 */
-	public static void addPerson(final Person person)
-	{
-		if (person != null)
-			filingSystem.put(person.getKey(), person);
-	}
+    /**
+     * Add a record to the database
+     *
+     * @param person - Record consisting of information pertaining to a person
+     *
+     * @return void
+     */
+    public static void addPerson(final Person person)
+    {
+        if (person != null)
+            filingSystem.put(person.getKey(), person);
+    }
 
-	/**
-	 * Method used to delete a record from the database
-	 *
-	 * @param key Object's identifier which equates to the object's name
-	 *
-	 * @return    Person object if located within database or null
-	 */
-	public static Person removePerson(final String key)
-	{
-		return filingSystem.remove(key);
-	}
+    /**
+     * Delete a record from the database
+     *
+     * @param key - Identifier which equates to the Person's name
+     *
+     * @return Person - Person object if located within database or null
+     */
+    public static Person removePerson(final String key)
+    {
+        return filingSystem.remove(key);
+    }
 
-	/**
-	 * Method used to retrieve a record from the database
-	 *
-	 * @param key Object's identifier which equates to the object's name
-	 *
-	 * @return    Person object if located within database or null
-	 */
-	public static Person retrievePerson(final String key)
-	{
-		return filingSystem.get(key);
-	}
+    /**
+     * Retrieve a record from the database
+     *
+     * @param key - Identifier which equates to the Person's name
+     *
+     * @return Person - Person object if located within database or null
+     */
+    public static Person retrievePerson(final String key)
+    {
+        return filingSystem.get(key);
+    }
 
-	/**
-	 * Method used to retrieve a specified sequence of records from the database
-	 *
-	 * @param filter Object used to filter the records for a specific member type
-	 *
-	 * @return       A specified sequence of records from the database if located
-	 *               or null
-	 */
-	public static Iterator<Person> getMemberInformation(Predicate filter)
-	{
-		List<Person> members = new ArrayList<Person>();
-		if (!filingSystem.isEmpty())
-		{
-			for (Person person : filingSystem.values())
-			{
-				if (filter.test(person))
-					members.add(person);
-			}
-			return members.iterator();
-		}
-		return null;
-	}
+    /**
+     * Retrieve a specified sequence of records from the database
+     *
+     * @param filter - Object used to filter the records for a specific member type
+     *
+     * @return List<Person> - A specified sequence of records from the database if located
+     *                        or null
+     */
+    public static List<Person> getMemberInformation(Predicate<Person> filter)
+    {
+        if (!filingSystem.isEmpty())
+        {
+            return filingSystem.values()
+                               .stream()
+                               .filter(person -> filter.test(person))
+                               .collect(Collectors.toList());
+        }
+        
+        return null;
+    }
 
-	/**
-	 * Method used to retrieve a copy of the database
-	 *
-	 * @return A copy of the database
-	 */
-	public static TreeMap<String, Person> getFilingSystem()
-	{
-		return new TreeMap<>(filingSystem);
-	}
+    /**
+     * Retrieve a copy of the database
+     *
+     * @return TreeMap<String, Person> - A copy of the database
+     */
+    public static TreeMap<String, Person> getFilingSystem()
+    {
+        return new TreeMap<>(filingSystem);
+    }
 
-	/**
-	 * Method used to provide data for the database
-	 *
-	 * @param data A map data structure containing database information
-	 *
-	 * @return     Whether the initialization was successful for not
-	 */
-	public static boolean setFilingSystem(Map<String, Person> data)
-	{
-		if ((filingSystem = data) != null)
-			return true;
-		else
-			return false;
-	}
+    /**
+     * Provide data for the database
+     *
+     * @param data - A map data structure containing database information
+     *
+     * @return boolean - true if initialization was successful, false otherwise
+     */
+    public static boolean setFilingSystem(Map<String, Person> data)
+    {
+        return ((filingSystem = data) != null) ? true : false;
+    }
 
-	/**
-	 * Method used to initialize the database for the application on startup
-	 */
-	public static void loadDirectory()
-	{
-		if (!Database.restore())
-		{
-			Directory.addPerson(new Student("Rico", "Felix",
-					"ricoantoniofelix@yahoo.com", "Freshman"));
-		}
-	}
+    /**
+     * Initialize the database for the application on startup
+     *
+     * @return void
+     */
+    public static void loadDirectory()
+    {
+        if (!Database.restore())
+            Directory.addPerson(new Student("Rico", "Felix",
+                    "ricoantoniofelix@yahoo.com", "Freshman"));
+    }
 
 }
